d01ed6cc9330b9f0a41a19df7f62feb59af87426
AllanBallester <allan.ballester@decathlon.com>
2017-08-23 14:18:55 -0300

    change email default

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'riserteam@gmail.com'
+  config.mailer_sender = 'risers_sweat@risekindness.com'
   config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
     scope: 'email',
     info_fields: 'email, first_name, last_name',
