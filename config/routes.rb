cf669c524a1ff84a79bf2bdffbb97e956e331f61
AllanBallester <allan.ballester@decathlon.com>
2017-08-28 16:23:42 -0300

    Internationalisation starting + Front Homepage 2 features

--- config/routes.rb
+++ config/routes.rb
@@ -9,22 +9,24 @@ Rails.application.routes.draw do
   devise_for :risers,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  get '/pages/our_story'
-  get '/pages/join_us'
-  get 'reviews/new'
+  scope '(:locale)', locale: /fr|es/ do
+    get '/pages/our_story'
+    get '/pages/join_us'
+    get 'reviews/new'
 
-root to: "pages#home"
+    root to: "pages#home"
 
- resources :homelesses, exclude: [:destroy] do
-    member do
-      post "create_interest"
+    resources :homelesses, exclude: [:destroy] do
+      member do
+        post "create_interest"
+      end
+      collection do
+        get "search"
+        get "retrieve"
+      end
+      resources :reviews, only: [:new, :create, :index, :update]
+      resources :photos, only: [:new, :create, :update]
     end
-    collection do
-      get "search"
-      get "retrieve"
-    end
-    resources :reviews, only: [:new, :create, :index, :update]
-    resources :photos, only: [:new, :create, :update]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
