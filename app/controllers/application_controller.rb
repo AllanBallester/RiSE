237dda4f63a26d604cf36de5f35868ad5713f4c9
AllanBallester <allan.ballester@decathlon.com>
2017-08-30 15:01:40 -0300

    Review Feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
   before_action :store_current_location, :unless => :devise_controller?
   before_action :set_locale
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
   def set_locale
     I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
   end
