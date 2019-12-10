714bdc30128d8a47bfb7eb30bf14e369e51b42d3
Allan Ballester <allan@tepee.pro>
2019-12-09 22:32:43 -0500

    lastsortingorder

--- app/controllers/homelesses_controller.rb
+++ app/controllers/homelesses_controller.rb
@@ -2,7 +2,7 @@ class HomelessesController < ApplicationController
   before_action :authenticate_riser!, only: [:new, :create, :edit]
 
   def index
-    @homelesses = Homeless.includes(:position_histories).order(updated_at: :desc).near("Paris", 10).first
+    @homelesses = Homeless.includes(:position_histories).order(updated_at: :desc).near(params[:location], 10)
 
     @hash = Gmaps4rails.build_markers(@homelesses) do |homeless, marker|
       marker.lat homeless.latitude
